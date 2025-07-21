8494238aa818bd7d0a7d23ee5837abcbf54b1a1f
Rochelle Awuor <awuorochelle75@gmail.com>
2025-07-21 10:13:40 +0300

    Adds the themes to tailwind.config

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,14 @@
+import { BrowserRouter, Router, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+
 function App() {
   return (
+    <BrowserRouter>
+    <Navbar />
     <div className="min-h-screen bg-gray-100 flex items-center justify-center">
       <h1 className="text-4xl font-bold text-blue-600">Smart Recruiter</h1>
     </div>
+    </BrowserRouter>
   )
 }
 
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import { Link,useNavigate, useLocation } from 'react-router-dom';
+import { Button } from './ui/button';
+import { useState } from 'react';
+
+
+function Navbar(){
+
+
+
+return (
+    <div>
+        
+    </div>
+);
+}
+
+export default Navbar;
