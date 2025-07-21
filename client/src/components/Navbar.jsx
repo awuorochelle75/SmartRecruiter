0f0d31eec8d834fab17bf5aa12a50f1ce5c704d5
Rochelle Awuor <awuorochelle75@gmail.com>
2025-07-21 11:34:22 +0300

    Adds initial navigation links and basic styling to navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,14 +4,22 @@ import { useState } from 'react';
 
 
 function Navbar(){
+ return (
+    <nav className="w-full bg-white-500 text-black p-5 flex justify-between ">
+        <div className='font-thin text-xl'>SmartRecruiter</div>
+        <div className = "space-x-10">
+            <Link to = "/">Home</Link>
+            <Link to = "/pricing">Pricing</Link>
+            <Link to = "/about">About</Link>
+              <Link to = "/about">Login</Link>
 
 
 
-return (
-    <div>
-        
-    </div>
-);
+        </div>
+     
+    </nav>
+  );
 }
+
 
 export default Navbar;
