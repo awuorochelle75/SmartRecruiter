8abdf0d6ec849e4bbd949271738fe7eb6c3fc298
IsaacDev14 <mwitiisaac14@gmail.com>
2025-07-21 10:58:18 +0300

    fix typo error in users table

--- server/app/models.py
+++ server/app/models.py
@@ -9,7 +9,7 @@ db = SQLAlchemy()
 class Users(db.Model):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nulluable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)  
     password_hash = db.Column(db.String(255), nullable=False)
     role =  db.Column(db.String(20), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
@@ -30,7 +30,7 @@ class Users(db.Model):
 
 
 # IntervieweeProfile
-class IntervieweeProfile(db.model):
+class IntervieweeProfile(db.Model):
     __tablename__= 'interviewee_profiles'
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id') , unique=True, nullable=False)
