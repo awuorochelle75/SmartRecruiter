210937356eb12fe23c132099a729a5a4d68ac614
IsaacDev14 <mwitiisaac14@gmail.com>
2025-07-21 09:59:21 +0300

    add users table

--- server/app/models.py
+++ server/app/models.py
@@ -1,4 +1,18 @@
+# app/models.py
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.dialects.postgresql import JSON, NUMERIC
+from datetime import datetime
+
 db = SQLAlchemy()
 
+# users
+class Users(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nulluable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
+    role =  db.Column(db.String(20), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    
 
