55a334e7776fc62a17f5b38c98651db073077866
IsaacDev14 <mwitiisaac14@gmail.com>
2025-07-21 10:12:02 +0300

    add interviewee_profiles table

--- server/app/models.py
+++ server/app/models.py
@@ -16,3 +16,21 @@ class Users(db.Model):
 
     
 
+
+# 2. IntervieweeProfile
+class IntervieweeProfile(db.model):
+    __tablename__= 'interviewee_profiles'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id') , unique=True, nullable=False)
+    first_name = db.Column(db.String(50))
+    last_name = db.Column(db.String(50))
+    phone = db.Column(db.String(20))
+    location = db.Column(db.String(100))
+    position = db.Column(db.String(100))
+    company = db.Column(db.String(100))
+    bio = db.Column(db.Text)
+    skills = db.Column(db.Text)
+    onboarding_completed = db.Column(db.Boolean, default=False)
+    avatar = db.Column(db.String(255))
+    resume_url = db.Column(db.String(255))
+
